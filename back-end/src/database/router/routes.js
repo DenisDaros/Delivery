a65e9896e4ace750123c61e934defe69b4256ecb
Fabiana Araujo <fnaraujo85@gmail.com>
2023-03-01 15:49:20 -0300

    feat: router products

--- back-end/src/database/router/routes.js
+++ back-end/src/database/router/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userController = require('../controller/user.controller')
+const productController = require('../controller/product.controller');
 
 const routers = express.Router();
 
-routers.post('/', userController.loginUser);
+routers.post('/login', userController.loginUser);
+routers.get('/products', productController.products);
 
 module.exports = routers;
