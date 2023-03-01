b4cfd0a416b9e231c0a557d9fc3c02e5920da39d
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2023-02-28 21:09:25 -0300

    fix: erros de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('../database/router');
-const cors = require('cors')
 const { loginUser } = require('../database/controller/user.controller');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-app.use('/', loginUser)
+app.use('/', loginUser);
 
 app.use(router.userRouter);
 
