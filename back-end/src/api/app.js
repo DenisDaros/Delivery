175f4a95ad6a5d49e65e64aa44d7539912628544
Victor <v_tord@hotmail.com>
2023-03-02 19:31:20 -0300

    feat: Images route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use('/images', express.static(`${__dirname}/../images`));
 app.use(routes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
