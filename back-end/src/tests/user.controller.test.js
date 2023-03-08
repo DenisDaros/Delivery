cfb4ced441c89aae0526cd93f2dd05d0a62a4404
Mirian <miriancordibelinogueira@gmail.com>
2023-03-08 19:07:32 -0300

    Salvando progresso dos testes

---
+++ back-end/src/tests/user.controller.test.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const { expect } = chai;
+
+const userController = require('../database/controller/user.controller');
+const userService = require('../database/services/user.service');
+
+describe('Testa user', function () {
+  describe('Verifica a rota de login', function () {
+    afterEach(function () {
+      sinon.restore();
+    });
+
+    it('Verifica se é possível logar com usuário que não existe', async function () {
+      const res = {};
+      const req = {
+        body: {
+          email: 'xablau@trybe.com',
+          password: '12345674',
+        },
+      };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon
+      .stub(userService, 'findUserByEmail')
+      .resolves(null);
+
+      await userController.loginUser(req, res);
+
+      expect(res.status.calledWith(404)).to.be.equal(true);
+    });
+
+    it('Verifica se é possível logar com usuário que existe', async function () {
+      const res = {};
+      const req = {
+        body: {
+          email: 'xablau@cordibeli.com',
+          password: '123456789',
+        },
+      };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon
+        .stub(userService, 'findUserByEmail')
+        .resolves({ password: '25f9e794323b453885f5181f1b624d0b' });
+
+      await userController.login(req, res);
+
+      expect(res.status.calledWith(200)).to.be.equal(true);
+    });
+  });
+});
---
+++ back-end/src/tests/user.service.test.js
@@ -0,0 +1,58 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { User } = require('../database/models/User');
+const userService = require('../database/services/user.service');
+
+describe('Testa user', function () {
+  describe('Verifica findUserById', function () {
+    // afterEach(function () {
+    //   sinon.restore();
+    // });
+
+    it('Verifica se é possível logar com usuário que não existe', async function () {
+      sinon.stub(User, 'findOne').resolves(null);
+
+      const response = await userService.findUserById({
+        email: 'xablu@trybe.com',
+      });
+
+      expect(response).to.deep.equal(404);
+    });
+  });
+
+  describe('Verifica create', function () {
+    afterEach(function () {
+      sinon.restore();
+    });
+
+    it('Verifica se o usuário já existe', async function () {
+      sinon.stub(User, 'findOne').resolves({
+        name: 'Mirian Teste Xablau',
+        email: 'xablau@tybe.com',
+        password: '123456789',
+      });
+
+      const response = await userService.create({
+        name: 'Mirian Test Xablau',
+        email: 'xablau@trybe.com',
+        password: '123456789',
+      });
+
+      expect(response).to.deep.equal(null);
+    });
+
+    it('Verifica se o usuário não existe', async function () {
+      sinon.stub(User, 'findOne').resolves(null);
+      sinon.stub(User, 'create').resolves({});
+
+      const response = await userService.create({
+        name: 'Mirian Teste Xablau',
+        email: 'xablau@trybe.com',
+        password: '123456789',
+      });
+
+      expect(response).to.deep.equal({});
+    });
+  });
+});
