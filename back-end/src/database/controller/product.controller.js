625c5cff4daeec25ac01c64ace4e8e1a13edc042
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2023-03-09 17:58:40 -0300

    fix: conflitos

--- back-end/src/database/controller/product.controller.js
+++ back-end/src/database/controller/product.controller.js
@@ -1,5 +1,4 @@
-const { findProducts, findProductById, updateProductById,
-destroyProduct, createProduct } = require('../services/product.service');
+const { findProducts, findProductById, updateProductById, destroyProduct, createProduct } = require('../services/product.service');
 
 const getProducts = async (_req, res) => {
   const result = await findProducts();
